5efb6feb114bc05e5f7dd2d424f456a31b49e25e
Observer <80362048+68756d616e@users.noreply.github.com>
2023-03-05 21:36:26 +0000

    Update # Basic website chatbot.py

--- # Basic website chatbot.py
+++ # Basic website chatbot.py
@@ -1,14 +1,20 @@
 # Basic website chatbot - Without Natural Langauge Learning models
 # In this the use will be a finance bot
 
+    # In the future - The below will display adjustable lists the owner can edit, to customise the information the users request
+#   AdjustableListOfQuestionA = []
+#   AdjustableListOfQuestionB = []
+#   AdjustableListOfQuestionC = []
+#   AdjustableListOfQuestionD = []
+
 # Loop
 while True:
 
-    # Greet customers
+    # Greet customers               
     print("Hello! I'm a Finance chatbot here to help. How can we assist you today?")
-
+    
     # Converse reasoning
-    print("We'd like to understand your reasons for communicating with us!")
+    print("We'd like to understand your reasons for communicating with us!")            
 
     # User question input
     user_question = input("""Please choose one of the following options so we can help assist you further!
@@ -20,17 +26,17 @@ Please enter the letter :          """)
 
     # User choice provided
     if user_question == 'a' or user_question == 'A':
-        print("You chose")
+        print("You chose A - Information about financial products and services")
 
     elif user_question == 'b' or user_question == 'B':
-        print("You chose")
+        print("You chose B - Investment advice  ")
     
     elif user_question == 'c' or user_question == 'C':
-        print("You chose")
+        print("You chose C - Educational resources ")
     
     elif user_question == 'd' or user_question == 'D':
-        print("You chose")
+        print("You chose D - Service inquiries")
 
     else:
-        print("Please choose A, B, C or D")
+        print("Please choose A, B, C or D") # Repeat of initial question
         print(user_question)
