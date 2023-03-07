587a4f9661f5ead1fdd4e704e7b515fef2c5eebd
Observer <80362048+68756d616e@users.noreply.github.com>
2023-03-07 17:04:15 +0000

    Update # Basic website chatbot.py

--- # Basic website chatbot.py
+++ # Basic website chatbot.py
@@ -1,6 +1,8 @@
 # Basic website chatbot - Without Natural Langauge Learning models
 # In this the use will be a finance bot
 
+# In the future I may include a Bot API, voice recognition and nlp
+
     # In the future - The below will display adjustable lists the owner can edit, to customise the information the users request
 #   AdjustableListOfQuestionA = []
 #   AdjustableListOfQuestionB = []
@@ -27,15 +29,104 @@ Please enter the letter :          """)
     # User choice provided
     if user_question == 'a' or user_question == 'A':
         print("You chose A - Information about financial products and services")
+        print("Please choose one of the following services!")
+        print() # Space
+        list_of_services = input("""List of Products and services!
+A - Investment services
+B - Financial planning services
+C - Financial education
+D - Accounting and tax service
+- Back
+""") # List of generic finance products and services
+        
+        print() # Space
+        if list_of_services == 'a' or list_of_services == 'A':
+            print("You chose Investment services")
 
-    elif user_question == 'b' or user_question == 'B':
+        elif list_of_services == 'b' or list_of_services == 'B':
+            print("You chose Financial planning services")
+
+        elif list_of_services == 'c' or list_of_services == 'C':
+            print("You chose Financial education")
+
+        elif list_of_services == 'd' or list_of_services == 'D':
+            print("You chose Accounting and tax service")
+            
+        elif list_of_services == 'Back' or list_of_services == 'back':
+            print(user_question)
+        
+        else:
+            print() # ...
+            break
+
+    elif a == 'b' or a == 'B':
         print("You chose B - Investment advice  ")
+        a= input()
+        
+        if a == 'a' or a == 'A':
+            print()
+
+        elif a == 'a' or a == 'A':
+            print()
+
+        elif a == 'a' or a == 'A':
+            print()
+
+        elif a == 'a' or a == 'A':
+            print()
+            
+        elif a == 'a' or a == 'A':
+            print()
+        
+        else:
+            print() # ...
+            break
     
-    elif user_question == 'c' or user_question == 'C':
+    elif b == 'c' or b == 'C':
         print("You chose C - Educational resources ")
+        b = input()
+        
+        if b == 'a' or b == 'A':
+            print()
+
+        elif b == 'a' or b == 'A':
+            print()
+
+        elif b == 'a' or b == 'A':
+            print()
+
+        elif b == 'a' or b == 'A':
+            print()
+            
+        elif b == 'a' or b == 'A':
+            print()
+        
+        else:
+            print() # ...
+            break
     
-    elif user_question == 'd' or user_question == 'D':
+    elif c == 'd' or c == 'D':
         print("You chose D - Service inquiries")
+        c = input()
+
+        if c == 'a' or c == 'A':
+            print()
+
+        elif c == 'a' or c == 'A':
+            print()
+
+        elif c == 'a' or c == 'A':
+            print()
+
+        elif c == 'a' or c == 'A':
+            print()
+            
+        elif c == 'a' or c == 'A':
+            print()
+        
+        else:
+            print() # ...
+            break
 
     else:
         print("Please choose A, B, C or D") # Repeat of initial question
